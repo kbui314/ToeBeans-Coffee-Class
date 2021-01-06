7324de70f077ea4292d1495c776d0755f1e23a69
Kenny Bui <kbui3145@gmail.com>
2021-01-06 14:23:05 -0700

    Fixed SignUp routing and bad crendential error

--- coffeeclass-front/src/components/signup/Signup.js
+++ coffeeclass-front/src/components/signup/Signup.js
@@ -1,95 +1,92 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import UserService from "../../services/UserService";
 import "./Signup.css";
 
 export default function SignUp() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [phone, setPhone] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const history = useHistory();
 
-    function validateForm() {
-        return (
-            firstName.length > 0 &&
-            lastName.length > 0 &&
-            email.length > 0 &&
-            password.length > 0 &&
-            phone.length > 0
-        );
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        let user = {
-            id: -1,
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            phone: phone,
-        };
-        console.log(user);
-        UserService.insertNewUser(user).then((response) => {
-            console.log(response);
-        });
-    }
-
+  function validateForm() {
     return (
-        <div className="SignUp">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="firstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control
-                        autoFocus
-                        type="firstName"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group size="lg" controlId="lastName">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                        type="lastName"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group size="lg" controlId="password">
-                    <Form.Label> Set Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group size="lg" controlId="phone">
-                    <Form.Label>Phone</Form.Label>
-                    <Form.Control
-                        type="phone"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    ></Form.Control>
-                </Form.Group>
-                <Button
-                    block
-                    size="lg"
-                    type="submit"
-                    disabled={!validateForm()}
-                >
-                    Sign Up
-                </Button>
-            </Form>
-        </div>
+      firstName.length > 0 &&
+      lastName.length > 0 &&
+      email.length > 0 &&
+      password.length > 0 &&
+      phone.length > 0
     );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let user = {
+      id: -1,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      phone: phone,
+    };
+    console.log(user);
+    UserService.insertNewUser(user).then((response) => {
+      history.push("/login");
+    });
+  }
+
+  return (
+    <div className="SignUp">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            autoFocus
+            type="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group size="lg" controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label> Set Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group size="lg" controlId="phone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control
+            type="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Sign Up
+        </Button>
+      </Form>
+    </div>
+  );
 }
--- coffeeclass-front/src/services/UserService.js
+++ coffeeclass-front/src/services/UserService.js
@@ -3,23 +3,23 @@ import axios from "axios";
 const API_URL = "http://localhost:8080";
 
 class UserService {
-    insertNewUser(user) {
-        return axios.post(`${API_URL}/users`, {
-            userId: 0,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.lastName,
-            password: user.password,
-            phone: user.phone,
-        });
-    }
+  insertNewUser(user) {
+    return axios.post(`${API_URL}/signup`, {
+      userId: 0,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.email,
+      password: user.password,
+      phone: user.phone,
+    });
+  }
 
-    loginUser(email, password) {
-        return axios.post(`${API_URL}/login`, {
-            username: email,
-            password: password,
-        });
-    }
+  loginUser(email, password) {
+    return axios.post(`${API_URL}/login`, {
+      username: email,
+      password: password,
+    });
+  }
 }
 
 export default new UserService();
